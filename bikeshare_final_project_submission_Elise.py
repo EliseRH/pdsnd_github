1317a9d55c63a3c1c118159df3b6cc375d2c2222
Elise Hoffmann <elise.hoffmann14@gmail.com>
2021-06-13 15:46:53 -0400

    added a note to print at the end of the user's interaction with the program

--- bikeshare_final_project_submission_Elise.py
+++ bikeshare_final_project_submission_Elise.py
@@ -216,6 +216,7 @@ def main():
         five_lines(city, month, day,df)
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
+            print("Thank you for exploring bikeshare data with me!")
             break
 
 if __name__ == "__main__":
